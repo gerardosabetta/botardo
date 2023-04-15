a90604b8c4f7d9589f2938c4f8cd53df72680091
Jerry (Gerardo Sabetta) <gerardo.sabetta@attackiq.com>
2023-04-15 01:04:36 -0300

    fix: fixes oauth issues

--- src/oauth.ts
+++ src/oauth.ts
@@ -4,25 +4,40 @@ import {
   APIGatewayProxyResult,
 } from "aws-lambda";
 import axios from "axios";
+import FormData from "form-data";
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   try {
-    if (event.queryStringParameters) {
-      await axios.post(
-        "https://slack.com/api/oauth.v2.access",
-        JSON.stringify({
-          client_id: process.env.CLIENT_ID,
-          client_secret: process.env.CLIENT_SECRET,
-          code: event.queryStringParameters.code,
-        })
-      );
+    if (!event.queryStringParameters) {
+      return {
+        body: "No code provided",
+        statusCode: 400,
+      };
     }
-    return {
-      statusCode: 200,
-      body: JSON.stringify("You can close this window now."),
-    };
+
+    const bodyFormData = new FormData();
+    bodyFormData.append("client_id", process.env.CLIENT_ID);
+    bodyFormData.append("client_secret", process.env.CLIENT_SECRET);
+    bodyFormData.append("code", event.queryStringParameters.code);
+
+    const response = await axios.post(
+      "https://slack.com/api/oauth.v2.access",
+      bodyFormData
+    );
+
+    if (response.data.ok) {
+      return {
+        statusCode: 200,
+        body: "App installed. You can close this window now",
+      };
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(response.data),
+      };
+    }
   } catch (error: any) {
     return {
       statusCode: 500,
