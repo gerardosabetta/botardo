19a257d58ba7b2d706484c73998e8726f006eeda
Matias Ciccone <32602795+MatiasCiccone@users.noreply.github.com>
2023-04-25 14:09:49 -0300

    Add 🔥 if variation exceeds a threshold

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
 export const capitalize = (s: string) =>
   s
-    .split(" ")
+    .split(' ')
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+    .join(' ');
+
+export const getName = ({ name, variation } = { name: string, variation: string }) => {
+  const capitalizedName = capitalize(name);
+
+  return Number(variation) >= 5 ? `${capitalizedName} 🔥` : capitalizedName;
+};
